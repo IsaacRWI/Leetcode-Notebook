5197a6d43e9333f6d19aff6cb0c24244d8c10fd9
IsaacNgRWI <isaacngrwi@gmail.com>
2025-09-01 20:43:40 +0800

    Added testing for two sum problem

---
+++ two sum testing.py
@@ -0,0 +1,8 @@
+map = {1, 2, 3, 4, 5}
+for i, n in enumerate(map):
+    print(i, n)
+
+my_list = ["apple", "banana", "cherry"]
+
+for index, item in enumerate(my_list):
+    print(f"Index: {index}, Item: {item}")

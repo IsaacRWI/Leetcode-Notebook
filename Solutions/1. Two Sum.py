4bed7475afd3bbbe4419f95569bd30e5ca748bd3
IsaacNgRWI <isaacngrwi@gmail.com>
2025-09-01 20:49:25 +0800

    Added explanation to two sum solution

--- Solutions/1. Two Sum.py
+++ Solutions/1. Two Sum.py
@@ -5,10 +5,10 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        map = {}
+        map = {}  # empty map for later use
 
         for i, n in enumerate(nums):  # enumerate allows for return of item and its index  i being index and n being the item
-            delta = target - n
-            if delta in map:
-                return [map[delta], i]
-            map[n] = i
+            delta = target - n  # delta ie the number we want, is the target (sum) - the item the for loop is on rn.  t=5 n=1 then x=5-1=4
+            if delta in map:  # if the value we're looking for is already in the map,
+                return [map[delta], i]  # then return it and the item in the for loop rn
+            map[n] = i  # else add the item into the empty map with its index from the original list

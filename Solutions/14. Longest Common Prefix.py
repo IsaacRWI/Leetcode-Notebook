f7d5b26793e90390bf88517cde3be8da5c0565c1
IsaacNgRWI <isaacngrwi@gmail.com>
2025-09-02 21:17:56 +0800

    Added explanation for Longest Common Prefix

--- Solutions/14. Longest Common Prefix.py
+++ Solutions/14. Longest Common Prefix.py
@@ -4,10 +4,10 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-        longest = ""
-        for i in range(len(strs[0])):
-            for s in strs:
-                if i == len(s) or s[i] != strs[0][i]:
-                    return longest
-            longest += strs[0][i]
-        return longest
+        longest = ""  # longest common prefix
+        for i in range(len(strs[0])):  # for every letter in the first word in the list
+            for s in strs:  # for every word in the string
+                if i == len(s) or s[i] != strs[0][i]:  # if the index is equal to length of word ie out of bounds or the ith letter for s != to the ith letter for the first word in list
+                    return longest  # return longest as output to function
+            longest += strs[0][i]  # if the ith letters equal, add the ith letter to longest
+        return longest  # if they are all equal then return longest

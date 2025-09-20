aa162201e6cc9dcebfd5e46d33b4ad19733e6c0e
Isaac <isaacngrwi@gmail.com>
2025-09-20 22:16:26 +0100

    Added explanations to binary search

--- Core/Binary Search 704.py
+++ Core/Binary Search 704.py
@@ -1,15 +1,15 @@
 def search( nums, target):
     l = 0
-    r = len(nums) - 1
-    while l <= r:
-        m = (l + r) // 2
-        if nums[m] > target:
+    r = len(nums) - 1  # -1 because index for list starts at 0
+    while l <= r:  # mean they haven't crossed each other yet
+        m = (l + r) // 2  # middle index
+        if nums[m] > target:  # shifts l or r depending on target position
             r = m - 1
         elif nums[m] < target:
               l = m + 1
         else:
-             return m
-    return -1
+             return m  # return m when nums[m] == target
+    return -1  # return -1 when l and r crossed and still didn't find target
 
 print(search([-1,0,3,5,9,12], 9))
 print(search([1,2,3],6))

19538f98b7b8f9318097ae80f4aef4b06514c20e
Isaac <isaacngrwi@gmail.com>
2025-09-20 21:30:01 +0100

    Added template for fizz buzz

---
+++ Core/FizzBuzz 412.py
@@ -0,0 +1,14 @@
+def fizzBuzz(n):
+    results = []
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            results.append("FizzBuzz")
+        elif i % 3 == 0:
+            results.append("Fizz")
+        elif i % 5 == 0:
+            results.append("Buzz")
+        else:
+            results.append(str(i))
+    return results
+
+print(fizzBuzz(30))

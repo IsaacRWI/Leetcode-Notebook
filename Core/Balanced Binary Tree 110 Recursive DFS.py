d83dea50d9563f7997eabd3f3157b8bc94b27aa9
Isaac <isaacngrwi@gmail.com>
2025-09-28 21:27:06 +0100

    Added template solution and explanation for recursive depth first search solution for balanced binary tree

---
+++ Core/Balanced Binary Tree 110 Recursive DFS.py
@@ -0,0 +1,19 @@
+class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+
+def isBalanced(self, root) -> bool:
+    def dfs(root):  # recursive function for depth first search
+        if not root:  # if root is null
+            return [True, 0]  # return true because even if its empty its balanced
+
+        left = dfs(root.left)  # recursive function for left leaves
+        right = dfs(root.right)
+
+        balance = (left[0] and right[0] and abs(left[1] - right[1]) <= 1)  # checking if the sub trees are balanced
+
+        return [balance, 1 + max(left[1], right[1])]  # return if its balanced and the height of the tree so far cleared
+
+    return dfs(root)[0]  # call recursive function on tree

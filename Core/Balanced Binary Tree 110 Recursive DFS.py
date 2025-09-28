6236495282995a84e903ad3593e8248eb6dc99cc
Isaac <isaacngrwi@gmail.com>
2025-09-28 21:28:09 +0100

    Updated explanation for balanced binary tree

--- Core/Balanced Binary Tree 110 Recursive DFS.py
+++ Core/Balanced Binary Tree 110 Recursive DFS.py
@@ -13,7 +13,7 @@ def isBalanced(self, root) -> bool:
         right = dfs(root.right)
 
         balance = (left[0] and right[0] and abs(left[1] - right[1]) <= 1)  # checking if the sub trees are balanced
-
+        # the height is for checking if the trees are balanced, if they have the same height +- 1 then theyre balanced
         return [balance, 1 + max(left[1], right[1])]  # return if its balanced and the height of the tree so far cleared
 
     return dfs(root)[0]  # call recursive function on tree

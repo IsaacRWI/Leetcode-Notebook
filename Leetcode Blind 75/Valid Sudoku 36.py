4ceca6ea244b1118b70c5db29146da6cb83a6488
Isaac <isaacngrwi@gmail.com>
2025-10-08 17:59:44 +0100

    Added more explanation on how the c pointer and square list works in Valid Sudoku 36.py

--- Leetcode Blind 75/Valid Sudoku 36.py
+++ Leetcode Blind 75/Valid Sudoku 36.py
@@ -9,9 +9,19 @@ def isValidSudoku(board) -> bool:
         for c in range(len(board[0])):  # for numbers in the list
             if board[r][c] == ".":  # if the value in the list is empty
                 continue  # skip the loop on this one and don't add it to the set
+
+    # basically the c index is the pointer going through all the values in the board
+    # if c is already in any of the lists ie the row(list) its in or column(list by index) or the square return False
+    # else add c to all 3 of these lists
+
             if (board[r][c] in rows[r] or  # if the number the pointer is on rn is in the sets rows
                 board[r][c] in cols[c] or  # columns
                 board[r][c] in square[(r // 3, c // 3)]):  # or squares
+
+                # the square set works by taking the indexes for rows and columns and divides them and rounds down by 3
+                # 0, 1, 2 //3 will be 0,  3, 4, 5 //3 will be 1, and 6, 7, 8 //3 will be 2
+                # it then takes the row and column square the pointer is in as the key for the dict and adds c index value into the set if it's not already there
+
                 return False
             # else
             cols[c].add(board[r][c])  # add the number to the set for column
